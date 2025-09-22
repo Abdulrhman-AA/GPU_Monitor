a927c2faee125639abb85b242e51a32fc10de1fb
Abdulrhman <d7king12@yahoo.com>
2025-09-22 19:44:53 +0300

    V1.1

--- GPU_MONITOR.py
+++ GPU_MONITOR.py
@@ -5,6 +5,7 @@ import time
 import csv
 from pynvml import *
 import os
+import win32gui
 
 
 def Get_new_logfile_name(base_name="GPU_m_log", extension=".csv", folder=""):
@@ -20,24 +21,39 @@ def Get_new_logfile_name(base_name="GPU_m_log", extension=".csv", folder=""):
         i += 1
 
 
-save_dirct = "C:/Users/D/Desktop/P/GPU Monitor/LOG_FILES"
+
+save_dirct = "C:/Users/D/Desktop/P/GPU Monitor/LOG_FILES" # Write the directory you wish to save the logged files to
 filename = Get_new_logfile_name(folder = save_dirct)
 full_path = os.path.join(save_dirct,filename)
 
-csvfile = open(full_path, mode = "w", newline = '')
+csvfile = open(full_path, mode = "w", newline = '', encoding="cp1252",errors="replace")
 csvwriter = csv.writer(csvfile)
-csvwriter.writerow(["Timestamp","GPU TEMP °", "Power Consumption W", "Utilization %", "GPU name"])
 
 print(f"\nLogging Data to : {filename} at : {save_dirct}\n")
 
 nvmlInit()
 
+handler = nvmlDeviceGetHandleByIndex(0)
+
+GPUname = nvmlDeviceGetName(handler)
+
+csvwriter.writerow(['','','','','','','','','','','','','',GPUname]) # Center the GPU name to the middle of the file
+
+#csvwriter.writerow([])
+
+
+csvwriter.writerow(["Timestamp","GPU TEMP °", "Power Consumption W", "Utilization %", "App"])
+
+
 try:
     while True:
-
-        handler = nvmlDeviceGetHandleByIndex(0)
-
-        GPUname = nvmlDeviceGetName(handler)
+    
+        last_title = None
+        hwnd = win32gui.GetForegroundWindow()
+        title = win32gui.GetWindowText(hwnd)
+        if title != last_title and title.strip():
+            last_title = title
+        time.sleep(1)
 
         temp = nvmlDeviceGetTemperature(handler,0)
 
@@ -46,11 +62,11 @@ try:
         util = nvmlDeviceGetUtilizationRates(handler)
 
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-        csvwriter.writerow([timestamp,temp,powercons,util.gpu,GPUname])
+        csvwriter.writerow([timestamp,temp,powercons,util.gpu,title])
         csvfile.flush()
 
         print(f"---------------------------------  {GPUname}  ---------------------------------")
-        print(f"GPU Temperature : {temp}° \nPower consumption : {powercons} W \nUtilization : {util.gpu} % \n---------------------------------------------------------------------------------------------")
+        print(f"GPU Temperature : {temp}° \nPower consumption : {powercons} W \nUtilization : {util.gpu} % \nTitle : {title} \n---------------------------------------------------------------------------------------------")
         time.sleep(2)
 except KeyboardInterrupt:
     print("Shutting down Monitoring.....\n")
